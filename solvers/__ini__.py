c6e8970f10c1b3b87fbf5bc2e11d83b19a9bc145
Vitaliy Pozdnyakov <pozdnyakov.vitaliy@yandex.ru>
2024-11-20 19:43:54 +0800

    add authorship and credits

---
+++ solvers/__ini__.py
@@ -0,0 +1,9 @@
+"""
+heterogat-scheduling-logistics
+
+Heterogeneous Graph Attention Networks for Scheduling in Cloud Manufacturing and Logistics
+"""
+
+__version__ = '0.0.1'
+__author__ = 'Vitaliy Pozdnyakov, Dmitrii Fomin'
+__credits__ = 'AIRI'
